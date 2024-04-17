3666960505c2830acaf7a767f21c63a29a2f7300
Roshni_Mahesh <roshnimahesh111@gmail.com>
2024-04-17 12:11:37 -0700

    complete word counter

---
+++ main.ts
@@ -0,0 +1,16 @@
+import inquirer from "inquirer";
+
+const answers: { Sentence: string } = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the words: "
+    }
+]);
+
+
+const words = answers.Sentence.trim().split(" ")
+
+console.log(words)
+
+console.log(`Your words in this sentence is ${words.length}`);
